359e984fd4a854d1244f27452ea03c669d3b5a3b
blackjack-ttp <phongtt120298@outlook.com>
2023-09-25 22:16:31 +0700

    #1 [setup] edit setting format

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      verbose: false,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  // Default and port default
+  base: '/',
+  server: {
+    port: 3000,
+  },
+
   resolve: {
     alias: {
       '@': '/src',
