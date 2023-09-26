432a175425871faad55a8f9642db8d4dd4546f09
blackjack-ttp <phongtt120298@outlook.com>
2023-09-26 21:04:55 +0700

    #2 [hook] useEffect create Exercise 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.scss';
-import Exercise1 from '@/components/UseEffect/Exercise1';
 // import Slide from '@/components/UseEffect/Exercise0';
+// import Exercise1 from '@/components/UseEffect/Exercise1';
+import Exercise2 from '@/components/UseEffect/Exercise2';
 
 function App() {
   return (
     <>
       <div className="App">
         {/* <Slide /> */}
-        <Exercise1 />
+        {/* <Exercise1 /> */}
+        <Exercise2 />
       </div>
     </>
   );
---
+++ src/components/UseEffect/Exercise2/index.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './style.scss';
+
+const Exercise2 = () => {
+  return (
+    <>
+      <section className="container">
+        <h1>Execrise 2</h1>
+      </section>
+    </>
+  );
+};
+
+export default Exercise2;
